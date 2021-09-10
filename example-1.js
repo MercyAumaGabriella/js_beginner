26d474ddf6ae9ae9270c2cdc1b8032e8ef059997
Mercy Auma <aumagabriella@gmail.com>
2021-09-10 08:40:47 +0300

    JavaScript for beginners

---
+++ example-1.js
@@ -0,0 +1,17 @@
+// let nam1 = 10;
+// var nam2;
+
+// //declared constant variable (single line comment)
+const grace = "raymond"
+let num = "200";
+let count = false;
+let empty;
+let object = null;
+let Val = 2;
+
+console.log(typeof(num));
+console.log(typeof(grace));
+console.log(typeof(count));
+console.log(typeof(empty));
+console.log(typeof(object));
+console.log("a"/2);
---
+++ operators.js
@@ -0,0 +1,8 @@
+//Operators in JavaScript.
+
+//1. Arithmetic Operators
+let num1 =100;
+let num2 = 50;
+
+let result = num1 + num2;
+console.log(result);
