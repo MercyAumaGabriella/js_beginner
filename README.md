e7e2fee5c1f011c774245f31bbbe6ac1830ed43c
Mercy Auma <aumagabriella@gmail.com>
2021-09-10 10:08:06 +0300

    explanation of increment operators. More code to operators.js file

--- README.md
+++ README.md
@@ -7,7 +7,24 @@
       - const
 
 2. operator.js
-   - This file conatains the basic knowledge about operators.
+   This file conatains the basic knowledge about operators.
+
    - ARITHMETIC OPERATORS
    This is used to apply mathematical instructions to values.
    These include addition(+), subtraction(-),multiplication(*), modulo(%) and division(/).
+
+   - INCREMENT OPERATORS
+   These operators increase the number of a variable by a given number. This operator uses the Addition operator(+)
+     - pre-increment
+       "let num3 = 5;
+        let result5 = ++num3;
+        let answer = num3"
+
+        Here, num3 = 5 before increment, result5 = 6 because we are applying increment before use and answer = 6 because it is used after increment.
+
+     - post increment
+       "let num3 = 5;
+        let result5 = num3++;
+        let answer = num3"
+
+        Here, num3 = 5 before increment, result5 = 5 because we are using the number value before increment and answer = 6 because it is used after increment.
--- operators.js
+++ operators.js
@@ -1,6 +1,6 @@
 //Operators in JavaScript.
 
-//1. Arithmetic Operators
+//1. ARITHMETIC OPERATORS
 let num1 =100;
 let num2 = 50;
 
@@ -23,3 +23,27 @@ console.log(result3);
 //Modulo operator
 let result4 = num1 % num2;
 console.log(result4);
+
+
+//2. INCREMENTAL OPERATORS
+
+//Addition increments
+//** post increment
+let num3 = 5;
+console.log(num3); // here num3 is still 5 beacuse no increment has been applied.
+let result5 = num3++;
+let answer = num3;
+
+console.log(num3); //here num = 6, because increment has been applied after.
+console.log(result5); //here result5 = 5 because the increment(1) is applied after use.
+console.log(answer);
+
+//** pre increment
+let num4 = 7;
+console.log(num4); //here num4 = 7, beacuse no increment has been applied.
+let result6 = ++num4;
+let answer1 = num4; 
+
+console.log(num4); //here num4 = 8, because increment is applied.
+console.log(result6); // here result6 = 8, because increment added before use.
+console.log(answer1); //here answer = 8 because increment had already been applied.
