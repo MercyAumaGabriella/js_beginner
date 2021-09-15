a37aa2554ec5f9c8213b258f9a0e99b076c23844
Mercy Auma <aumagabriella@gmail.com>
2021-09-15 10:34:30 +0300

    Introducing arrays with arrays.js. Here, the basic Knowledge of arrays is talked about

---
+++ arrays.js
@@ -0,0 +1,17 @@
+//ARRAYS
+let my_array = [20, 30, 40, 50, 60];
+console.log(my_array[0]);
+
+//using a loop to look through our array
+for( let i=0; i<=4; i++){
+    //console.log(my_array[i]);
+}
+
+//Odd numbers
+for( let j=0; j<=4; j++){
+    let x = my_array[j];
+    if (x%2 == 0){
+        console.log("Odd number");
+        console.log(my_array[j]);
+    }
+}
