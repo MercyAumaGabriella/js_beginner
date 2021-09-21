9f301704e4b6d80b95337204c88b2a75206469a0
Mercy Auma <aumagabriella@gmail.com>
2021-09-21 13:21:05 +0300

    More on classes. Tuesday work

--- class.js
+++ class.js
@@ -39,12 +39,54 @@ const president = {
 }
 
 //2. DYNAMIC CLASS
-function animal ( name, color, type, gender ){
+function animal ( name, color, type, gender, adjective){
     this.name = name; 
     this.color = color;
     this.type = type;
     this.gender = gender;
+    this.adjective = adjective;
+    this.cry = function(){
+        return this.name + ' ' + 'has a sound!';
+    }
+    this.story = function() {
+        return this.name + ' is a ' + this.adjective + ' animal.';
+    }
 }
 
 let animal1 = new animal('elephant', 'grey','wild','male');
-let animal2 = new('dog','brown','domestic','female');
+let animal2 = new animal('dog','brown','domestic','female','bright');
+
+//new methods being called for the object instances.
+console.log(animal1.cry());
+console.log(animal2.story());
+
+//seven classes with a minimum of seven properties with atleast two methods with atleast three instances (NOT BEYOND TWO PM)
+//put comments in your work
+
+//ARROW FUNCTIONS
+function woman (name, height, color, tribe, character, style){
+    this.name = name;
+    this.height = height;
+    this.color = color;
+    this.tribe = tribe;
+    this.character = character;
+    this.style = style;
+    this.laugh = (cry_style) =>{
+        return this.name + ' '+'laughs'+' '+ cry_style;
+    }
+}
+
+let woman1 = new woman('Grace', 'tall', 'light-skinned','Muganda', 'Principled', 'sophisticated');
+console.log(woman1.laugh('bubbly'));
+
+const man = function(name, facial, voice){
+    this.name = name;
+    this.facial = facial;
+    this.voice = voice;
+    this.care = (percent) => {
+        return this.name + ' is '+ percent + ' when it comes to care';
+    }
+}
+
+let man1 = new man('Manny', 'absent', 'base');
+console.log(man1.care(10 + '%'));
