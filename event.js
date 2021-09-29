7e9e49c50990d7f1a1f48eabaab7f47f0735cbdc
Mercy Auma <aumagabriella@gmail.com>
2021-09-29 13:34:02 +0300

    Adding more code about event listeners

--- event.js
+++ event.js
@@ -1,16 +1,40 @@
 //Changing the text content and text color within a paragragh
-const changeText = () => {
-    const pr = document.querySelector('p');
-    //pr.textContent = "I changed because of an inline event handler.";
+// const changeText = () => {
+//     const pr = document.querySelector('p');
+//     //pr.textContent = "I changed because of an inline event handler.";
 
-    //displaying the text in a div
-    const div = document.querySelector('div');
-    div.style.color = "green";
-    div.textContent = "I changed because of an inline event handler.";
-    div.style.border = "1px solid darkgrey";
+//     //displaying the text in a div
+//     const div = document.querySelector('div');
+//     div.style.color = "green";
+//     div.textContent = "I changed because of an inline event handler.";
+//     div.style.border = "1px solid darkgrey";
+// }
+
+// const clear = () => {
+//     const pClear = document.getElementById('div');
+//     pClear.textContent.clear;
+// }
+
+//event listener
+const button1 = document.getElementById("button");
+const button2 = document.getElementById('button2');
+const p = document.getElementById("pr");
+
+const Listener = () => {
+    p.textContent = "Hey there, I am an event listener."
 }
 
-const clear = () => {
-    const pClear = document.getElementById('div');
-    pClear.textContent.clear;
+//the function is a callback
+button1.addEventListener('click',Listener);
+
+//ALERT
+const alert_text = () => {
+    alert('Alerted Because of an Event Listener');
 }
+
+const btn2 = () => {
+   confirm('form submitted');
+}
+
+//button2.addEventListener('dbclick',alert_text);
+button2.addEventListener('focus',btn2);
